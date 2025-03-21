9ac36bab69db443cec7cfdb6ef2123af164337d2
gilbertkristiaan <gilbertkristian79@gmail.com>
2025-03-21 10:28:31 +0700

    refactor : remove few newlines in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,8 @@ type Job = Box<dyn FnOnce() + Send + 'static>;
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-
         let (sender, receiver) = mpsc::channel();
-
         let receiver = Arc::new(Mutex::new(receiver));
-        
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
